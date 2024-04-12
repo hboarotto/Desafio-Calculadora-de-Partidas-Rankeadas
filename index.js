cc9f66ea2dff43659aa5023f1fa2bb58f97f3dd3
hboarotto <hboarotto@gmail.com>
2024-04-12 12:08:08 +0100

    primeiros calculos

---
+++ index.js
@@ -0,0 +1,19 @@
+// inicio do desafio de Calculadora de Partidas Rankeadas 
+
+
+// criando variaveis 
+
+
+
+
+
+// criando função 
+
+function somar(numVitorias , numDerrotas){
+    return  numVitorias - numDerrotas;
+}
+let saldoVitorias = somar(10 , 5)
+ console.log( "O resultado é" + saldoVitorias )
+// Mensagem de saída 
+
+//console.log("O Herói tem de saldo de " + saldoVitorias + "está no nível de  " + nivel)
